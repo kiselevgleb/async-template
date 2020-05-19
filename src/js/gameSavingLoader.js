a561988f8ef6b4fbf78d7ea9d40e893a2a6b3d64
kiselevgleb <kiselevgleb@gmail.com>
2020-05-19 23:12:13 +0300

    added catch error

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -1,11 +1,14 @@
 'use strict';
 import read from './reader';
 import json from './parser';
-
 export default class GameSavingLoader {
   static async load() {
+    try {
     const readData = await read(); 
     const jsonData = await json(readData); 
-    return jsonData;
+    return jsonData;}
+    catch {
+      return "error";
+    }
   }
 }
